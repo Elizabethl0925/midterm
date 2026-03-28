4a43e0ac46e5c06ba6aac434e2b4700dac865bf5
Elizabethl0925 <lizziebabegirl2020@gmail.com>
2026-03-28 16:10:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-window.addEventListener("scroll", () => {
-  const nav = document.getElementById("navbar");
-  nav.style.background = window.scrollY > 50
-    ? "rgba(10,12,18,0.95)"
-    : "rgba(10,12,18,0.7)";
-});
+
