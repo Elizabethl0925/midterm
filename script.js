e9599da843d0b1fa0398424b9a0a6e4c05434a10
Elizabethl0925 <lizziebabegirl2020@gmail.com>
2026-03-28 14:53:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
 
+toggle.addEventListener("click", function(e) {
+  e.preventDefault();
+  menu.classList.toggle("active");
+});
